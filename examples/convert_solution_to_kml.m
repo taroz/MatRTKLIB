d3497aba8e52f3f908e9d4f9535137e653888c86
Taro Suzuki <taroz1461@gmail.com>
2024-06-24 11:53:19 +0900

    Add examples/plot_position.m and examples/convert_solution_to_kml.m

---
+++ examples/convert_solution_to_kml.m
@@ -0,0 +1,16 @@
+clear; clc; close all;
+addpath ..\
+datapath = ".\data\kinematic\";
+
+%% Read RTKLIB solution file
+gsol = gt.Gsol(datapath+"rover_rtk.pos");
+
+%% Convert solution to KML file
+open = 1; % Open Google Earth (0:off,1:on)
+lw = 1;    % Line width (0: No line)
+lc = "w";  % Line Color (e.g. "r" or [1 0 0])
+ps = 0.5; % Point size (0: No point)
+pc = 0; % Point Color (0: Solution status)
+
+% Save KML file
+gsol.outKML(datapath+"rover_rtk.kml", open, lw, lc, ps, pc);
---
+++ examples/plot_position.m
@@ -0,0 +1,15 @@
+clear; clc; close all;
+addpath ..\
+
+%% Generate position
+enu = zeros(11,3); enu(:,1) = 0:100:1000;
+orgxyz = [-3959340.203 3352854.274 3697471.413]; % position origin in ECEF
+
+pos = gt.Gpos(enu,"enu");
+pos.setOrg(orgxyz,"xyz");
+
+%% Plot position
+pos.plot
+pos.plotMap
+pos.plotSatMap
+
